68a27e0e1fb4ccd71effa846a67345b71cb83246
Jean-Marcellin Truong <jeanmarcellintruong@gmail.com>
2018-09-28 20:38:32 +0200

    yolo

--- yolo.sh
+++ yolo.sh
@@ -7,12 +7,17 @@ git clone https://aur.archlinux.org/yaourt.git
 cd yaourt
 yes | makepkg -si
 cd ..
-'
 
-yaourt -Syu bspwm zsh alacritty oh-my-zsh-git polybar sddm qt5 ttf-fira-mono i3lock rofi sxhkd feh mpd compton
+rm -rf package-query
+rm -rf yaourt
+
+yaourt -Sy --noconfirm bspwm zsh alacritty oh-my-zsh-git polybar sddm qt5 ttf-fira-mono i3lock rofi sxhkd feh mpd compton
 
 git clone https://github.com/Eayu/sddm-theme-clairvoyance
 sudo mv sddm-theme-clairvoyance /usr/share/sddm/themes/clairvoyance
+'
+
+
 mkdir -p ~/.config/
 cp -r ./.config/ ~/.config/
 
