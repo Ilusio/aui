d3bd7837f21e1ce6fa1df0a8dc501c5a9e99986f
Jean-Marcellin Truong <jeanmarcellintruong@gmail.com>
2018-10-09 22:28:39 +0200

    Update yolo.sh

--- yolo.sh
+++ yolo.sh
@@ -11,7 +11,7 @@ cd ..
 rm -rf package-query
 rm -rf yaourt
 
-yaourt -Sy --noconfirm xorg-server xorg-xinit bspwm zsh alacritty oh-my-zsh-git polybar sddm qt5 ttf-fira-mono i3lock rofi sxhkd feh mpd compton xorg-xrandr dina-font ttf-font-awesome mpc gnome-calendar alsa-utils dunstify dunst light ncmpcpp lxappearance materia-gtk-theme paper-icon-theme okular wps-office udisks2 udiskie ntfs-3g vifm intellij-idea-community-edition
+yaourt -Sy --noconfirm xorg-server xorg-xinit bspwm zsh alacritty oh-my-zsh-git polybar sddm qt5 ttf-fira-mono i3lock rofi sxhkd feh mpd compton xorg-xrandr dina-font ttf-font-awesome mpc gnome-calendar alsa-utils dunstify dunst light ncmpcpp lxappearance materia-gtk-theme paper-icon-theme okular wps-office udisks2 udiskie ntfs-3g vifm intellij-idea-community-edition python2-distribute libspotify
 
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
@@ -19,7 +19,7 @@ cd fonts
 cd ..
 rm -rf fonts
 
-'
+
 
 git clone https://github.com/Eayu/sddm-theme-clairvoyance
 mkdir -p /usr/share/sddm/themes/
@@ -27,12 +27,14 @@ sudo mv sddm-theme-clairvoyance /usr/share/sddm/themes/clairvoyance
 sudo cp ./sddm.conf /etc/
 sudo cp ./Background.jpg /usr/share/sddm/themes/clairvoyance/Assets
 
+'
 
 mkdir -p ~/.config/
 cp -r ./.config/ ~
 cp -r ./.scripts ~
 cp ./.Xresources ~
 cp ./.xinitrc ~
+cp ./.fehbg ~
 
 mkdir -p ~/Pictures
 cp -r ./Wallpaper ~/Pictures/
@@ -51,6 +53,7 @@ cd ..
 
 cp .zshrc .zsh_plugins.txt ~
 
+chsh -s /usr/bin/zsh
 localectl set-x11-keymap fr
 
 systemctl enable sddm.service
